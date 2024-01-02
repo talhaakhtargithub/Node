30641642a2775bd1d204a4c0d333528d5d2dd68c
Talha Akhtar <talhaakhtarkpr1@gmail.com>
2024-01-02 22:51:46 +0500

    Changes

--- Learning/crud.js
+++ Learning/crud.js
@@ -3,12 +3,25 @@ const path = require('path');
 const dirPath = path.join(__dirname, "crud");
 const filePath = `${dirPath}/apple.txt`;  // Corrected variable name to dirPath
 
+
+//>> File Created
+
 // fs.writeFileSync(filePath, 'This is me Talha');
+
+
+//>> File Read
+
 
 // fs.readFile(filePath,'utf8',(arr,item)=>{
 //     console.log(item)
 // })
 
-fs.appendFile(filePath,' And file name is apple.txt',(err)=>{
-    if(!err){console.log("File is Updated");}
+//>> File Updated
+// fs.appendFile(filePath,' And file name is apple.txt',(err)=>{
+//     if(!err){console.log("File is Updated");}
+// })
+
+//>> File Name is Updated
+fs.rename(filePath,`${dirPath}/frui.txt`,(err)=>{
+    if(!err){console.log("FileName is Updated");}
 })
