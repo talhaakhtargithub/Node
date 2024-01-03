165f18db40df67b0ae8dbbaae3dc1970d411c597
Talha Akhtar <talhaakhtarkpr1@gmail.com>
2024-01-03 14:56:29 +0500

    OK

--- Express/4-routing.js
+++ Express/4-routing.js
@@ -1,26 +1,44 @@
 const express = require('express')
 const app = express()
-const { products, people } = require('./express tutorial/3-data_json')
+const { products, people } = require('./3-data_json')
 
 app.get('/', (req, res) => {
     res.status(200).send(`<h1>Home Page</h1> <div><a href="/api/products">products</a></div>`)
 
 })
+
+
+
+
 app.get('/api/products', (req, res) => {
     const newProducts = products.map((product) => {
         const { name, price, size } = product
         return { name, price, size }
     })
 
-    res.status(200).json(newProducts)
+    
+    const peoples2 = people.map((peoples) => {
+        const { id, name } = peoples
+        return { id, name }
+    })
+    const responseData = {
+        products: newProducts,
+        people: peoples2
+    };
+
+    res.json(responseData);
 })
+
+
+
+
 app.get('/api/products/:id', (req, res) => {
     const requestedId = Number(req.params.id);
 
     // Filter products based on the requested ID and it returns an array 
     const matchedProducts = products.filter((product) => {
         return product.id === requestedId;
-    });
+    }); 8
 
     if (matchedProducts.length > 0) {
         // If products are found, return the matched product
@@ -30,6 +48,12 @@ app.get('/api/products/:id', (req, res) => {
         res.send(`<h1>Product not found</h1>`);
     }
 });
+
+
+
+
+
+
 app.get('/api/v1/products/query', (req, res) => {
     console.log(req.query);
     const { search, id } = req.query;
@@ -52,12 +76,17 @@ app.get('/api/v1/products/query', (req, res) => {
         res.json(...findItem)
     }
     else {
-       notFound()
+        notFound()
     }
-    function notFound (){
+    function notFound() {
         res.send(`<h1>Resource not found !!! </h1>`)
     }
 })
+
+
+
+
+
 
 app.listen(5000, () => {
     console.log('Server is listening to the port 5000....');
